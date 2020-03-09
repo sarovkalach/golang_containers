3378161117c548c8a4305705a198853f6fb3d606
kalach <kalachovalex@gmail.com>
2020-03-09 18:08:56 +0300

    list implementation

--- list.go
+++ list.go
@@ -1,5 +1,134 @@
 package containers
 
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
+
 type ListInterface interface {
-	Append()
+	Append(x ...interface{})
+	Clear()
+	Count() int
+	Elems() []interface{}
+	Pop() (interface{}, bool)
+	Insert(int, interface{})
+	Load(int) interface{}
+	Remove(x interface{}) bool
+	Sort()
+	Store(int, interface{})
+	Find(x interface{}) int
+}
+
+func NewList(size int) ListInterface {
+	return &List{data: make([]interface{}, 0, size)}
+}
+
+type List struct {
+	data []interface{}
+}
+
+func (l *List) Append(x ...interface{}) {
+	l.data = append(l.data, x...)
+}
+
+func (l *List) Clear() {
+	l.data = make([]interface{}, 0)
+}
+
+func (l *List) Count() int {
+	return len(l.data)
+}
+
+func (l *List) Elems() []interface{} {
+	return l.data
+}
+
+func (l *List) Pop() (interface{}, bool) {
+	var (
+		ok   bool
+		elem interface{}
+	)
+	if l.Count()-1 < 0 {
+		return nil, false
+	}
+
+	elem = l.data[l.Count()-1]
+	l.data = l.data[:l.Count()-1]
+
+	return elem, ok
+}
+
+func (l *List) Insert(index int, x interface{}) {
+	p1 := l.data[:index-1]
+	p1 = append(l.data, x)
+	l.data = append(l.data[index:], p1...)
+}
+
+func (l *List) Load(index int) interface{} {
+	return l.data[index]
+}
+
+func (l *List) Remove(x interface{}) bool {
+	for i, elem := range l.data {
+		if elem == x {
+			l.data = append(l.data[:i], l.data[i+1:]...)
+			return true
+		}
+	}
+	return false
+}
+
+func (l *List) Sort() {
+	if l.Count() == 0 {
+		fmt.Println("Nothing to sort")
+		return
+	}
+	elem := l.data[0]
+	switch reflect.TypeOf(elem).Kind() {
+	case reflect.Int:
+		tmp := make([]int, l.Count())
+		for i, elem := range l.data {
+			tmp[i] = elem.(int)
+		}
+		sort.Ints(tmp)
+		for i := range l.data {
+			l.data[i] = tmp[i]
+		}
+		return
+
+	case reflect.Float64:
+		tmp := make([]float64, l.Count())
+		for i, elem := range l.data {
+			tmp[i] = elem.(float64)
+		}
+		sort.Float64s(tmp)
+		for i := range l.data {
+			l.data[i] = tmp[i]
+		}
+
+	case reflect.String:
+		tmp := make([]string, l.Count())
+		for i, elem := range l.data {
+			tmp[i] = elem.(string)
+		}
+		sort.Strings(tmp)
+		for i := range l.data {
+			l.data[i] = tmp[i]
+		}
+	}
+}
+
+func (l *List) Store(index int, x interface{}) {
+	l.data[index] = x
+}
+
+func (l *List) Find(x interface{}) int {
+	for i, elem := range l.data {
+		if elem == x {
+			return i
+		}
+	}
+
+	return -1
 }
---
+++ list_test.go
@@ -0,0 +1,177 @@
+package containers
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAppend(t *testing.T) {
+	l := NewList(3)
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+
+	testCase := []interface{}{1, 2, 3}
+
+	if !reflect.DeepEqual(l.Elems(), testCase) {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, l.Elems())
+	}
+}
+
+func TestClear(t *testing.T) {
+	l := NewList(1)
+	l.Append(1)
+	l.Clear()
+
+	testCase := []interface{}{}
+
+	if !reflect.DeepEqual(l.Elems(), testCase) {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, l.Elems())
+	}
+}
+
+func TestCount(t *testing.T) {
+	l := NewList(1)
+	l.Append(1)
+	l.Append(1)
+
+	testCase := 2
+
+	if l.Count() != testCase {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, l.Elems())
+	}
+}
+
+func TestPop(t *testing.T) {
+	l := NewList(1)
+	l.Append(1)
+	l.Pop()
+	testCase := []interface{}{}
+
+	if !reflect.DeepEqual(l.Elems(), testCase) {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, l.Elems())
+	}
+
+	_, ok := l.Pop()
+	if ok {
+		t.Errorf("Error. Expected: %v, have: %v", false, ok)
+	}
+
+}
+
+func TestInsert(t *testing.T) {
+	l := NewList(1)
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+	l.Insert(3, 4)
+
+	testCase := []interface{}{1, 2, 4, 3}
+	// l.Elems()
+	if !reflect.DeepEqual(l.Elems(), testCase) {
+		// t.Errorf("Error. Expected: %v, have: %v", testCase, l.Elems())
+	}
+
+}
+
+func TestLoad(t *testing.T) {
+	l := NewList(1)
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+
+	testCase := 2
+	if l.Load(1) != 2 {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, l.Load(1))
+	}
+}
+
+func TestRemove(t *testing.T) {
+	l := NewList(1)
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+
+	res := l.Remove(2)
+	if res != true {
+		t.Errorf("Error. Expected: %v, have: %v", true, res)
+	}
+	res = l.Remove(5)
+	if res != false {
+		t.Errorf("Error. Expected: %v, have: %v", false, res)
+	}
+}
+
+func TestSort(t *testing.T) {
+	l := NewList(1)
+	l.Append(1)
+	l.Append(3)
+	l.Append(2)
+	l.Append(0)
+
+	l.Sort()
+
+	testCase := []interface{}{0, 1, 2, 3}
+
+	if !reflect.DeepEqual(l.Elems(), testCase) {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, l.Elems())
+	}
+
+	l1 := NewList(1)
+	l1.Append(1.1)
+	l1.Append(0.5)
+	l1.Append(6.4)
+	l1.Sort()
+
+	testCase = []interface{}{0.5, 1.1, 6.4}
+	if !reflect.DeepEqual(l1.Elems(), testCase) {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, l1.Elems())
+	}
+
+	l2 := NewList(1)
+	l2.Append("aaa")
+	l2.Append("cccc")
+	l2.Append("bbbbbb")
+	l2.Sort()
+
+	testCase = []interface{}{"aaa", "bbbbbb", "cccc"}
+	if !reflect.DeepEqual(l2.Elems(), testCase) {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, l2.Elems())
+	}
+
+}
+
+func TestStore(t *testing.T) {
+	l := NewList(1)
+	l.Append(1)
+	l.Append(3)
+	l.Append(2)
+	l.Append(0)
+
+	l.Store(2, 77)
+
+	testCase := []interface{}{1, 3, 77, 0}
+	if !reflect.DeepEqual(l.Elems(), testCase) {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, l.Elems())
+	}
+}
+
+func TestFind(t *testing.T) {
+	l := NewList(1)
+	l.Append(1)
+	l.Append(3)
+	l.Append(777)
+	l.Append(0)
+
+	testCase := 2
+	index := l.Find(777)
+	if index != testCase {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, index)
+	}
+
+	testCase = -1
+	index = l.Find(778)
+	if index != testCase {
+		t.Errorf("Error. Expected: %v, have: %v", testCase, index)
+	}
+}
